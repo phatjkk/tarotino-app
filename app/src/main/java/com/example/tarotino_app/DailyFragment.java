4cae96cee0a551402083651e1b6d7e4148526557
phatjk <autoittutorial1234@gmail.com>
2022-12-11 19:48:39 +0700

    Thêm chức năng lấy card bên tab tarot hằng ngày

--- app/src/main/java/com/example/tarotino_app/DailyFragment.java
+++ app/src/main/java/com/example/tarotino_app/DailyFragment.java
@@ -1,6 +1,10 @@
 package com.example.tarotino_app;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,7 +12,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.io.InputStream;
 
 
@@ -17,11 +28,56 @@ public class DailyFragment extends Fragment {
     public DailyFragment(){
         // require a empty public constructor
     }
-
+    public JSONObject JSON_Parse(String s){
+        try {
+            JSONObject cardObject = new JSONObject(s);
+            return cardObject;
+        }
+        catch (Exception e){
+            return null;
+        }
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_daily, container, false);
+        Button btnGetCard = (Button) view.findViewById(R.id.view_card_btn);
 
-        return inflater.inflate(R.layout.fragment_daily, container, false);
+        Tarot tarot = new Tarot(getActivity());
+        String cardJSONDataString = tarot.getRandomCard();
+
+        JSONObject cardJSONDataObj = JSON_Parse(cardJSONDataString);
+        try {
+            String cardImageUrl = cardJSONDataObj.getString("image_url").substring(1);
+            ImageView i;
+            Bitmap bm = getBitmapFromAsset(cardImageUrl);
+            i = (ImageView)view.findViewById(R.id.img_card);
+            i.setImageBitmap(bm);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        btnGetCard.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+                Intent intent = new Intent(getActivity(), ViewCardActivity.class);
+                intent.putExtra("CardJSON", cardJSONDataString);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
+    private Bitmap getBitmapFromAsset(String strName) throws IOException
+    {
+        AssetManager assetManager = getActivity().getAssets();
+        InputStream istr = assetManager.open(strName);
+        Bitmap bitmap = BitmapFactory.decodeStream(istr);
+        return bitmap;
+    }
+
 }
